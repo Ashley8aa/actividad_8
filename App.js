98945b4502fc119b961e88e5e29efd31db99a526
Ashley8aa <a1118120096@utch.edu.mx>
2022-03-11 19:25:43 -0700

    Actividad 8 primera version

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [datos, setDatos] = useState([]);
+  const [isLoading, setloading] = useState(true);
+
+
+
+  const getPosts = async () => {
+    try { 
+      const url = "https://jsonplaceholder.typicode.com/posts"; 
+
+    //Consumo de datos
+    const response =  await fetch(url);
+    //Convertir a json
+    const json = await response.json();
+    setDatos(json);
+    } catch (error) {
+    console.error(error);
+   } finally {
+     setloading(false);
+   }
+
+    }
+    
+
+  useEffect(() => {
+    getPosts(); 
+  }, []) 
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      { isLoading ?  <ActivityIndicator /> : (  //Es true, Si no es verdadero, muestra el flatlist
+      
+
+        <FlatList 
+        data = {datos}
+        keyExtractor = { ({id}, index) => id}
+        renderItem = {
+          ({item}) => (
+            <Text>{item.title} </Text>
+          )
+        }
+        /> 
+        ) 
+      }
+     
     </View>
   );
 }
